36871b05217ca14855fe7d430d07fcce1b204ba0
suraj9060 <87435542+suraj9060@users.noreply.github.com>
2022-08-05 19:20:06 +0530

    model created

---
+++ server/models/productSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema({
+    id: String,
+    url: String,
+    detailUrl: String,
+    title: Object,
+    price: Object,
+    quantity: Number,
+    description: String,
+    discount: String,
+    tagline:String
+})
